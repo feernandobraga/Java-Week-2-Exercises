b7b76802de46fce17ef1dde890b7a5630b52d033
feernandobraga <fernando.bragga@gmail.com>
2019-08-06 13:11:17 +1000

    Added the temperature exercise

--- src/TemperatureConverter.java
+++ src/TemperatureConverter.java
@@ -6,4 +6,18 @@ import java.util.Scanner;
 
 public class TemperatureConverter {
 
+    public static void main(String[] args) {
+        Scanner userIO = new Scanner(System.in);
+
+        float celsius;
+        float fahrenheit;
+        float convertedTemperature;
+
+        System.out.println("Enter the temperature in Celsius: ");
+        celsius = userIO.nextFloat();
+
+        convertedTemperature = (celsius * 9/5) + 32;
+        System.out.println("The equivalent temperature in Fahrenheit is: " + convertedTemperature);
+    }
+
 }
