e912232c423b2a876731d28080b3e33d3e87772c
feernandobraga <fernando.bragga@gmail.com>
2019-08-06 11:53:45 +1000

    Added the temperature exercise

---
+++ src/TemperatureConverter.java
@@ -0,0 +1,9 @@
+//Exercise 1.2 Write a Java program covered in class to convert degrees Celsius to
+//degrees Fahrenheit.
+//Hint: Use the formula F = C*9/5 + 32
+
+import java.util.Scanner;
+
+public class TemperatureConverter {
+
+}
