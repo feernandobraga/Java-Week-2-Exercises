31e69853280065ba7bddb71261cae08a9f7e8c36
feernandobraga <fernando.bragga@gmail.com>
2019-08-06 11:34:22 +1000

    initial commit

---
+++ src/difference.java
@@ -0,0 +1,26 @@
+// Exercise 1.1 Write a java program to display the result of difference of two numbers.
+// A user will enter two integers ( import Scanner)
+// Program will calculate their difference
+// Program then display the result
+
+import java.util.Scanner;
+
+public class difference {
+
+    public static void main(String[] args) {
+        int firstNumber;
+        int secondNumber;
+        Scanner userIO = new Scanner(System.in);
+
+        System.out.println("Please enter the first number");
+        firstNumber = userIO.nextInt();
+
+        System.out.println("Please enter the second number");
+        secondNumber = userIO.nextInt();
+
+        System.out.println("The difference between " + firstNumber + " and " + secondNumber + " is: " + (firstNumber - secondNumber));
+
+
+    }
+
+}
